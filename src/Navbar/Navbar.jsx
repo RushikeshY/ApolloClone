aed95e746a98c1c1819bbfc65f67e7d0fe689dc1
RushikeshY <99939510+RushikeshY@users.noreply.github.com>
2022-05-07 13:17:57 +0530

    Pharmacy and Cart Final

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,19 +1,6 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom'
-// const Navbar = () => {
-//   return (
-//     <div>
-
-// <Link className='gap' to="/pharmacy">Pharmacy</Link>
-
-//     </div>
-//   )
-// }
-import { IoIosCart } from "react-icons/io";
-// export default Navbar
 
 import React from "react";
-// import "../styles/navbar.css";
+
 import "../Pharmacy/navbar.css"
 import { Link } from "react-router-dom";
 
@@ -21,20 +8,9 @@ const Navbar = ({ setShow, size }) => {
   return (
 
     <nav>
-    <Link to="/pharmacy">Pharmacy</Link>
-    <Link className='gap' to="/cart">Cart</Link>
-      {/* <div className="nav_box">
-        <span  style={{marginLeft:'100px',margin:"30px", fontSize : "26px" , FontWeight:"bold", "padding":"10px"}} className="my_shop" onClick={() => setShow(true)}>
-          <h3>Pharmacy</h3>
-        </span>
-        <div className="cart" onClick={() => setShow(false)}>
-          <span>
-        
-            <i  style={{marginLeft:'700px', backgroundColor:"red", fontSize : "26px" , FontWeight:"bold","padding":"10px"}} className="fas fa-cart-plus"></i>
-          </span>
-          <span>{size}</span>
-        </div>
-      </div> */}
+      <Link to="/pharmacy">Pharmacy</Link>
+      <Link className='gap' to="/cart">Cart</Link>
+
     </nav>
   );
 };
--- src/Pharmacy/Cart.jsx
+++ src/Pharmacy/Cart.jsx
@@ -1,155 +1,50 @@
-// import React, { useState, useEffect } from "react";
-// // import "../styles/cart.css";
-// import "./Cart.css"
-// const Cart = (handleClick) => {
-//   // const [Cost, setCost] = useState(0);
+import React, { useEffect, useState } from "react";
+import "./Cart.css";
+const Cart1 = () => {
+  const [details, setDetails] = useState([]);
 
-//   // const handleRemove = (id) => {
-//   //   const arr = cart.filter((item) => item.id !== id);
-//   //   setCart(arr);
-//   //   handleCost();
-//   // };
+  const getData = async () => {
+    let res = await fetch("http://localhost:8080/CartData");
+    let data = await res.json();
+    setDetails(data);
+    // console.log(data);
+  };
 
-//   // const handleCost = () => {
-//   //   let ans = 0;
-//   //   cart.map((item) => (ans += item.amount * item.Cost));
-//   //   setCost(ans);
-//   // };
+  useEffect(() => {
+    getData();
+  }, []);
 
-//   // useEffect(() => {
-//   //   handleCost();
-//   // });
-//   const [cart,setCart]=useState([])
-//   const getData=async()=>{
-//     let res = await fetch("http://localhost:3000/CartData")
-//     let data=await res.json()
-//     setCart(data)
-//     // console.log(data)
-//   }
-
-//   useEffect(()=>{
-//     getData()
-//   },[])
-  
-  
-//   const onDelete = (id) => {
-
-//     let newTodo = cart.filter((item) => item.id !== id);
-//     setCart(newTodo);
-//   };
-  
-//   const Delete = async (id) => {
-
-//     try {
-//       await fetch(`http://localhost:3000/CartData/${id} `, {
-//         method: "DELETE",
-//         headers: { "content-type": "application/json" },
-//       });
-
-//       console.log(2)
-//       onDelete(id);
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-// console.log(cart)
-//   return (
-//     <article>
-//       {cart.map((item) => (
-//         <div className="cart_box" key={item.id}>
-//             <div className="cart_img">
-//               <img src={item.url} alt="" />
-//               <h3 id="h3">{item.Name}</h3>
-//             </div>
-//               {/* <div>
-//                 <button onClick={() => handleChange(item, 1)}>+</button>
-//                 <button>{item.amount}</button>
-//                 <button onClick={() => handleChange(item, -1)}>-</button>
-//               </div>
-//               <div>
-//                 <span>{item.Cost}</span>
-//                 <button onClick={() => handleRemove(item.id)}>Remove</button>
-//               </div> */}
-//         </div>
-//       ))}
-//       <button onClick={() => Delete(cart.id)}>Delete</button>
-//       <div className="total">
-//         <span style={{fontSize:"22px",color:"teal",fontWeight:"bold"}}>Total Cost of your Cart</span>
-//         <span style={{fontSize:"22px",fontWeight:"bold",color:"green"}}> Rs - {cart.Cost}</span>
-//       </div>
-//     </article>
-//   );
-// };
-
-// export default Cart;
-
-
-
-
-
-import React, { useEffect, useState } from 'react'
-// import Navbar from './NAvbar'
-
-
-const Cart1= () => {
-  
-  const [details,setDetails]=useState([])
-  const getData=async()=>{
-    let res = await fetch("http://localhost:8080/CartData")
-    let data=await res.json()
-    setDetails(data)
-    console.log(data)
-  }
-
-  useEffect(()=>{
-    getData()
-  },[])
-  
-
-
-
-
-
-
-  
-  
   const onDelete = (id) => {
-
     let newTodo = details.filter((item) => item.id !== id);
     setDetails(newTodo);
   };
-  
-  const Delete = async (id) => {
 
+  const Delete = async (id) => {
     try {
       await fetch(`http://localhost:8080/CartData/${id} `, {
         method: "DELETE",
         headers: { "content-type": "application/json" },
       });
 
-      console.log(2)
+      console.log(2);
       onDelete(id);
     } catch (e) {
       console.log(e);
     }
   };
 
-
   const [Cost, setCost] = useState(0);
-
-
 
   const handleCost = () => {
     let ans = 0;
     details.map((item) => (ans += item.amount * item.Cost));
     setCost(ans);
-    console.log(Cost)
+    console.log(Cost);
   };
 
   useEffect(() => {
     handleCost();
   });
-
 
   const handleChange = (item, d) => {
     const ind = details.indexOf(item);
@@ -158,46 +53,100 @@ const Cart1= () => {
 
     if (arr[ind].amount === 0) arr[ind].amount = 1;
     setDetails([...arr]);
- 
   };
-  
 
   return (
     <div>
       {/* <Navbar/> */}
+      <h3>My Cart</h3>
+      <hr />
+      <div style={{ display: "flex" }}>
+        <p className="p">Add address to confirm item availability</p>
+        <h4 style={{ color: "#ff960d", paddingLeft: "110px" }}>
+          + Login To Continue
+        </h4>
+      </div>
 
-      <div>
-        {
-          details.map((item)=>{
-            return( 
-                <div className='data'>
-                  <div>
-                    <div>
-                      <img className='img' src={item.url} alt="" />
+      <hr />
+
+      <div style={{ display: "flex", width: "100%" }}>
+        <div style={{ width: "70%" }}>
+          {details.map((item) => {
+            return (
+              <div>
+                <div style={{ width: "160%" }}>
+                  <div className="Cart_page">
+                    {/* <hr /> */}
+                    <div style={{ disaplay: "flex", width: "100%" }}>
+                      <div
+                        style={{
+                          display: "flex",
+                          color: "rgb(13, 77, 104)",
+                          fontSize: "18px",
+                          paddingLeft: "10px",
+                          width: "90%",
+                        }}
+                      >
+                        <div style={{ width: "80%", display: "flex" }}>
+                          <img className="img" src={item.url} alt="" />
+                          <p style={{ paddingLeft: "50px" }}>
+                            <b>{item.Name}</b>
+                          </p>
+                        </div>
+
+                        <div style={{ width: "20%", marginLeft: "50px" }}>
+                          <button
+                            className="remove_btn"
+                            onClick={() => Delete(item.id)}
+                          >
+                            Remove
+                          </button>
+                        </div>
+                      </div>
+
+                      <div className="plus" style={{ display: "flex" }}>
+                        <button onClick={() => handleChange(item, 1)}>+</button>
+                        <button>{item.amount}</button>
+                        <button onClick={() => handleChange(item, -1)}>
+                          -
+                        </button>
+                        <h3 style={{ paddingLeft: "30px" }}>
+                          MRP Rs.{item.Cost}
+                        </h3>
+                      </div>
                     </div>
-                    <p >{item.Name}</p>
-                    <p>{item.Discription}</p>
-                     <p>{item.Cost}</p>
-
-                    <div>
-            <button onClick={() => handleChange(item, 1)}>+</button>
-            <button>{item.amount}</button>
-            <button onClick={() => handleChange(item, -1)}>-</button>
-            <h5>${Cost}</h5>
-          </div>
-
-
-
-                    <button onClick={() => Delete(item.id)}>Delete</button>
+                    {/* <hr /> */}
                   </div>
+                </div>
               </div>
-            )
-          })
-        }
+            );
+          })}
+          <h3 style={{ paddingLeft: "40px" }}>Total Cost : {Cost}</h3>
+        </div>
+
+        <div className="Right_cartPage">
+          <h3 style={{ fontSize: "21px" }}>CART BREAKDOWN</h3>
+          <hr />
+          <hr />
+          <h3 style={{ paddingLeft: "40px" }}>Total Cost : {Cost}</h3>
+          <p style={{ paddingLeft: "40px", color: "rgb(13, 77, 104)" }}>
+            Tax Amount : Rs.{Cost / 10}
+          </p>
+          <p style={{ paddingLeft: "40px", color: "rgb(13, 77, 104)" }}>
+            Delivary Charges : Rs.{Cost / 20}
+          </p>
+          <hr />
+
+          <h3 style={{ fontSize: "22px", paddingLeft: "40px" }}>
+            Amount To pay : Rs. {Math.ceil(Cost + Cost / 10 + Cost / 30)}
+          </h3>
+
+          <button className="payment1">Proceed To Payment</button>
+          <hr />
+        </div>
       </div>
-      </div>
-    )
-    
-}
+    </div>
+  );
+};
 
 export default Cart1;
